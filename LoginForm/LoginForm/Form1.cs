fd100b51a72edfe221a14f88c818f234b4946606
YeonHui Kim <imgimyoni@gmail.com>
2021-08-07 22:46:52 +0900

    간단한 로그인 폼

---
+++ LoginForm/LoginForm/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LoginForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text == "홍길동")
+            {
+                if (textBox2.Text == "1234")
+                {
+                    MessageBox.Show("로그인에 성공하였습니다.");
+                }
+                else
+                    MessageBox.Show("로그인에 실패하였습니다.");
+            }
+            else
+                MessageBox.Show("없는 계정입니다");
+        }
+    }
+}
